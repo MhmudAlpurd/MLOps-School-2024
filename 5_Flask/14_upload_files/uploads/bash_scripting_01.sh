4fdfc01ed10973b03972771ae0dc0ba3e5995aad
MhmudAlpurd <Mahmoudalipour.D@Gmail.Com>
2024-11-28 20:49:55 +0330

    ci cd

---
+++ 5_Flask/14_upload_files/uploads/bash_scripting_01.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Scripting is so important"
+
--- 5_Flask/19_smart_web/app.py
+++ 5_Flask/19_smart_web/app.py
@@ -115,4 +115,4 @@ def result():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
